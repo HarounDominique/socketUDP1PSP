5a409b6e0ea769d250614e900a6e867850881a9c
HarounDominique <haroundominique@gmail.com>
2023-04-12 21:48:40 +0200

    Initial commit

---
+++ src/ClienteUDP.java
@@ -0,0 +1,39 @@
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class ClienteUDP {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+
+        InetAddress destino = InetAddress.getLocalHost();
+        int port = 12345; //puerto al que enviar el mensaje, el del servidor
+        DatagramSocket socket = new DatagramSocket(34567); //Puerto local, el del cliente
+        String texto = "";
+        do {
+            //contenedor del mensaje más o menos
+            byte[] mensaje = new byte[1024];
+
+            //mensaje, digamos, en bruto
+            System.out.printf("Introduce una cadena de texto:");
+            texto = scan.nextLine();
+            mensaje = texto.getBytes();
+
+            //Datagram a ENVIAR:
+            DatagramPacket envio = new DatagramPacket(mensaje, mensaje.length, destino, port);
+
+
+            //Envío de datagram:
+            socket.send(envio);
+
+            byte[] bufer = new byte[1024]; //bufer que recibirá el datagram del cliente
+            //esperando al datagram:
+            System.out.println("Esperando al datagrama...");
+            DatagramPacket paqueteRecibir = new DatagramPacket(bufer, bufer.length);
+            socket.receive(paqueteRecibir); //recibiendo datagrama
+            System.out.println(paqueteRecibir);
+
+        }while(!texto.equals("*"));
+        socket.close();
+    }
+}
---
+++ src/ServidorUDP.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+
+public class ServidorUDP {
+    public static void main(String[] args) throws IOException {
+        byte[] bufer = new byte[1024]; //bufer que recibirá el datagram del cliente
+        DatagramSocket socket = new DatagramSocket(1234);
+
+        //esperando al datagram:
+        System.out.println("Esperando al datagrama...");
+        DatagramPacket paqueteRecibir = new DatagramPacket(bufer, bufer.length);
+        socket.receive(paqueteRecibir); //recibiendo datagrama
+
+        //int bytesRecibidos = paqueteRecibir.getLength(); //obtengo el número de bytes
+        String paqueteDef = new String(paqueteRecibir.getData()); //obtengo String
+        String mayus = paqueteDef.toUpperCase();
+
+        //creo contenedor
+        byte[] mensajeMayus = new byte[1024];
+        //meto mensaje en contenedor
+        mensajeMayus = mayus.getBytes();
+        DatagramPacket envio = new DatagramPacket(mensajeMayus, mensajeMayus.length, paqueteRecibir.getAddress(), paqueteRecibir.getPort());
+
+        //Envío de datagram:
+        socket.send(envio);
+
+    }
+}
